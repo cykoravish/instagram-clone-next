a845cd104443d28308c18260b7bb5f46022d26d4
Ravish Bisht <ravishbisht85@gmail.com>
2024-05-30 19:52:38 +0530

    mini profile made

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,17 +1,19 @@
-import MiniProfile from './MiniProfile';
-import Posts from './Posts';
+import MiniProfile from "./MiniProfile";
+import Posts from "./Posts";
 
 export default function Feed() {
   return (
-    <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
       {/* Posts (Left) */}
-      <section className='md:col-span-2'>
+      <section className="md:col-span-2">
         <Posts />
       </section>
 
       {/* MiniProfile (Right) */}
-      <section className='hidden md:inline-grid md:col-span-1'>
-        <MiniProfile />
+      <section className="hidden md:inline-grid md:col-span-1">
+        <div className="fixed w-[380px]">
+          <MiniProfile />
+        </div>
       </section>
     </main>
   );
--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+
 export default function MiniProfile() {
-    return (
-      <div>MiniProfile</div>
-    )
-  }
+  const { data: session } = useSession();
+  return (
+    <div className="flex items-center justify-between mt-14 scroll-ml-10">
+      <Image
+        width={64}
+        height={64}
+        src={session?.user?.image || "/800px-Instagram_logo_2016.webp"}
+        alt="user-profile-pic or instagram logo"
+        className="rounded-full border p-[2px]"
+      />
+      <div className="flex-1 ml-4">
+        <h2 className="font-bold">{session?.user?.username}</h2>
+        <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+      </div>
+      {session ? (
+        <button
+          onClick={signOut}
+          className="text-blue-500 text-sm font-semibold"
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          onClick={signIn}
+          className="text-blue-500 text-sm font-semibold"
+        >
+          Sign In
+        </button>
+      )}
+    </div>
+  );
+}
