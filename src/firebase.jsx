d651e4c11a96cde090d5cd7436d0b258ff56e936
Ravish Bisht <ravishbisht85@gmail.com>
2024-05-30 15:45:12 +0530

    integrating firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "insta-next-24536.firebaseapp.com",
+  projectId: "insta-next-24536",
+  storageBucket: "insta-next-24536.appspot.com",
+  messagingSenderId: "283779343751",
+  appId: "1:283779343751:web:1daa5d3aebcaaab6715f22"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+// service firebase.storage {
+//     match /b/{bucket}/o {
+//       match /{allPaths=**} {
+//         allow read;
+//         allow write: if
+//         request.resource.size < 2 * 1024 * 1024 &&
+//         request.resource.contentType.matches('image/.*')
+//       }
+//     }
+//   }
