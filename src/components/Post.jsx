f29189d90f4358bd9a13dd248b32b5928cbda5ab
Ravish Bisht <ravishbisht85@gmail.com>
2024-05-30 20:41:09 +0530

    add like functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,26 @@
-import { HiOutlineDotsVertical } from 'react-icons/hi';
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import LikeSection from "./LikeSection";
 
 export default function Post({ post }) {
   return (
-    <div className='bg-white my-7 border rounded-md'>
-      <div className='flex items-center p-5 border-b border-gray-100'>
+    <div className="bg-white my-7 border rounded-md">
+      <div className="flex items-center p-5 border-b border-gray-100">
         <img
           src={post.profileImg}
           alt={post.username}
-          className='h-12 rounded-full object-cover border p-1 mr-3'
+          className="h-12 rounded-full object-cover border p-1 mr-3"
         />
-        <p className='flex-1 font-bold'>{post.username}</p>
-        <HiOutlineDotsVertical className='h-5 cursor-pointer' />
+        <p className="flex-1 font-bold">{post.username}</p>
+        <HiOutlineDotsVertical className="h-5 cursor-pointer" />
       </div>
       <img
         src={post.image}
         alt={post.caption}
-        className='object-cover w-full'
+        className="object-cover w-full"
       />
-
-      <p className='p-5 truncate'>
-        <span className='font-bold mr-2'>{post.username}</span>
+      <LikeSection id={post.id} />
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{post.username}</span>
         {post.caption}
       </p>
     </div>
